4eab17290cd2cc95b36459d387ff86f851eafdb8
xuannhat1203 <141288976+xuannhat1203@users.noreply.github.com>
2024-01-31 19:23:31 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,216 +1,155 @@
-let ip1 = document.getElementById("ip1");
-let ip2 = document.getElementById("ip2");
-let ip3 = document.getElementById("ip3");
-let ip4 = document.getElementById("ip4");
-let show = document.getElementById("show");
 let heroInfo = {
     name: "Minh Cường",
-    job: "freelance react developer",
+    job: "Freelance react developer",
     imgUrl: "https://picsum.photos/200/300",
-};
-let personalDetail = {
+  };
+  
+  let personalDetail = {
     name: "Minh Cường",
     dob: "06 June 1995",
     spokenLanguages: ["English", "France", "German"],
     nationality: "Vietnam",
     interest: ["Music", "Reading", "Journey"],
     techs: [
-    {
+      {
         id: 1,
         imgUrl: "https://i.postimg.cc/cHdfNH2Z/android.png",
         techName: "Android",
         exp: 2,
-    },
-    {
+      },
+      {
         id: 2,
         imgUrl: "https://i.postimg.cc/nrCjHQk8/Angular.png",
         techName: "Angular",
         exp: 1,
-    },
-    {
+      },
+      {
         id: 3,
         imgUrl: "https://i.postimg.cc/G3VJ4csP/bootstrap.png",
         techName: "Bootstrap",
         exp: 3,
-    },
-    {
+      },
+      {
         id: 4,
         imgUrl: "https://i.postimg.cc/dt91z6v7/vue.png",
         techName: "Vue",
         exp: 2,
-    },
-    {
+      },
+      {
         id: 5,
         imgUrl: "https://i.postimg.cc/XNdXg3zk/react.png",
         techName: "React",
         exp: 2 / 3,
-    },
-    {
+      },
+      {
         id: 6,
         imgUrl: "https://i.postimg.cc/PfgYt2B2/mongodb.png",
         techName: "Mongodb",
         exp: 0.25,
-    },
-    {
+      },
+      {
         id: 7,
         imgUrl: "https://i.postimg.cc/RZzFYYjx/laravel.png",
         techName: "Laravel",
         exp: 1,
-    },
-    {
+      },
+      {
         id: 8,
         imgUrl: "https://i.postimg.cc/X7N3ybSJ/nodejs-icon.png",
         techName: "Node.js",
         exp: 5 / 6,
-    },
+      },
     ],
-};
-let array = [
+  };
+  
+  let projects = [
     {
-        id: 1,
-        name:" Auto Drive Project",
-        img: `https://cplusplus.com`,
-        link:`https://cplusplus.com`,
-        tag: `
-            <button>Angular</button>
-            <button>React</button>
-            <button>Jquery</button>
-            `,
-        button:
-        `
-            <button>Delete</button>
-            <button>Update</button>
-        `
+      id: 1,
+      imgUrl: "https://i.postimg.cc/nrCjHQk8/Angular.png",
+      projectName: "Auto Drive Project",
+      link: "https://abcd-example.com",
+      tags: ["Angular", "React", "Jquery"],
     },
     {
-        id: 2,
-        name:"Ecommerce Project",
-        img: `https://cplusplus.com`,
-        link:`https://cplusplus.com`,
-        tag: `
-            <button>Angular</button>
-            <button>React</button>
-            <button>Jquery</button>
-            `,
-        button:
-        `
-            <button id="del">Delete</button>
-            <button id="update">Update</button>
-        `
+      id: 2,
+      imgUrl: "https://i.postimg.cc/G3VJ4csP/bootstrap.png",
+      projectName: "Ecommerce Project",
+      link: "https://abcd-example.com",
+      tags: ["Bootstrap", "CSS", "Javascript"],
     },
     {
-        id: 3,
-        name:" Chat Application",
-        img: `https://cplusplus.com`,
-        link:`https://cplusplus.com`,
-        tag: `
-            <button>Angular</button>
-            <button>React</button>
-            <button>Jquery</button>
-            `,
-        button:
-        `
-            <button>Delete</button>
-            <button>Update</button>
-        `
+      id: 3,
+      imgUrl: "https://i.postimg.cc/PfgYt2B2/mongodb.png",
+      projectName: "Chat Application",
+      link: "https://abcd-example.com",
+      tags: ["MongoDB", "Javascript"],
     },
     {
-        id: 4,
-        name:" Social media Platform",
-        img: `https://cplusplus.com`,
-        link:`https://cplusplus.com`,
-        tag: `
-            <button>Angular</button>
-            <button>React</button>
-            <button>Jquery</button>
-            `,
-        button:
-        `
-            <button>Delete</button>
-            <button>Update</button>
-        `
+      id: 4,
+      imgUrl: "https://i.postimg.cc/dt91z6v7/vue.png",
+      projectName: "Social Media Platform",
+      link: "https://abcd-example.com",
+      tags: ["Vue", "Javascript"],
     },
     {
-        id: 5,
-        name:" Image Sharing Platform",
-        img: `https://cplusplus.com`,
-        link:`https://cplusplus.com`,
-        tag: `
-            <button>Angular</button>
-            <button>React</button>
-            <button>Jquery</button>
-            `,
-        button:
-        `
-            <button>Delete</button>
-            <button>Update</button>
-        `
+      id: 5,
+      imgUrl: "https://i.postimg.cc/XNdXg3zk/react.png",
+      projectName: "Image Sharing Platform",
+      link: "https://abcd-example.com",
+      tags: ["React", "Javascript"],
     },
 ];
-localStorage.setItem('personalDetail', JSON.stringify(personalDetail));
-localStorage.setItem('array', JSON.stringify(array));
-localStorage.setItem('heroInfo', JSON.stringify(heroInfo));
-localStorage.getItem('personalDetail', JSON.parse(personalDetail));
-localStorage.getItem('array', JSON.parse(array));
-localStorage.getItem('heroInfo', JSON.parse(heroInfo));
-let header = document.getElementById("text");
-let li = document.createElement("li");
-li.innerHTML = `
-        <h3 id"h3">${heroInfo.name}</h3>
-        <h1>${heroInfo.job}</h1>
+localStorage.setItem("hero", JSON.stringify(heroInfo));
+localStorage.setItem("person", JSON.stringify(personalDetail));
+localStorage.setItem("projects", JSON.stringify(projects));
+JSON.parse(localStorage.getItem("hero"));
+let name2 = document.getElementById("name");
+let job = document.getElementById("job");
+name2.textContent = `${heroInfo.name}`;
+job.textContent = `${heroInfo.job}`;
+let tex1= document.getElementsByClassName("text1");
+let tex2 = document.getElementsByClassName("text2");
+let tex3 = document.getElementsByClassName("text3");
+let tex4 = document.getElementsByClassName("text4");
+text1.textContent = `: ${personalDetail.dob}` ;
+text2.textContent = `: ${personalDetail.spokenLanguages}`;
+text3.textContent = `: ${personalDetail.nationality}`;
+text4.textContent = `: ${personalDetail.interest}`;
+localStorage.setItem("techs", JSON.stringify(personalDetail.techs));
+let choice = document.getElementsByClassName("choice1");
+Array.from(choice).forEach((choiceItem, index) => {
+    if (index < personalDetail.techs.length) {
+        let tech = personalDetail.techs[index];
+        let text = document.createElement("div");
+        text.classList.add("text_icon");
+        text.innerHTML = `
+            <h1>${tech.techName}</h1>
+            <p>${tech.exp} Year Experience</p>
         `;
-header.appendChild(li);
-let button = document.createElement("button");
-button.innerHTML = "DOWNLOAD CV";
-header.appendChild(button);
-ip1.value = personalDetail.dob;
-ip4.value = personalDetail.spokenLanguages;
-ip2.value = personalDetail.nationality;
-ip3.value = personalDetail.interest;
-let renderTechs = () => {
-    let techListElement = document.querySelector("#ss2 .top");
-    let techListElement1 = document.querySelector("#ss2 .bot");
-    personalDetailLocal.techs.forEach((tech, index) => {
-        let techItem = document.createElement('div');
-        techItem.className = 'item1';
-        techItem.innerHTML = `
-            <div class="icon">
-                <img src="${tech.imgUrl}" width="150px" height="110px">
-            </div>
-            <div class="content">
-                <h3>${tech.techName}</h3>
-                <p>${tech.exp} Years Experience</p>
-            </div>`;
-        if (index < 5) {
-            techListElement.appendChild(techItem);
-        } else {
-            techListElement1.appendChild(techItem);
-        }
-    });
-};
-let item = document.createElement("div");
-item.className = "item";
-let itemLeft = document.createElement("div");
-itemLeft.className = "item_left";
-let img = document.createElement("img");
-img.src = array[i].img;
-itemLeft.appendChild(img);
-let itemRight = document.createElement("div");
-itemRight.className = "item_right";
-let name = document.createElement("h3");
-name.textContent = array[i].name;
-let link = document.createElement("a");
-link.href = array[i].link;
-link.textContent = array[i].link;
-let span = document.createElement("span");
-span.className = "material-symbols-outlined";
-span.textContent = "open_in_new";
-itemRight.appendChild(name);
-itemRight.appendChild(link);
-itemRight.appendChild(span);
-item.appendChild(itemLeft);
-item.appendChild(itemRight);
-item_top.appendChild(item);
+        choiceItem.appendChild(text);
+    }
+});
+let projectItems = document.getElementsByClassName("star1");
+Array.from(projectItems).forEach((projectItem, index) => {
+    if (index < projects.length) {
+        let projectName = projects[index].projectName;
+        let link = projects[index].link;
+        let linkElement = document.createElement("div");
+        linkElement.innerHTML = `
+            <h1>${projectName}</h1>
+            <p>${link}</p>
+            <a href="${link}"><span class="material-symbols-outlined">open_in_new</span></a>
+        `;
+        projectItem.querySelector(".link").appendChild(linkElement);
+        let btnElement = document.createElement("div");
+        btnElement.innerHTML = `
+            <button>ANGULAR</button>
+            <button>REACT</button>
+            <button>JAVASCRIPT</button>
+        `;
+        projectItem.querySelector(".btn_star").appendChild(btnElement);
+    }
+});
 
 
 
